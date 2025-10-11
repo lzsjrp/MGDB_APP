1aa65f9f6ff72b51eb3f852931aa42d4e4354dcc
Felipe Eliseu <felipe.eliseu@unesp.br>
2025-10-11 14:01:23 -0300

    change default page to favorites

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -57,7 +57,7 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
 
   final List<Widget> _pages = [
     const ExplorePage(),
