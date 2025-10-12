9722bcea1e73caafc9858873f578930664c23000
Felipe Eliseu <felipe.eliseu@unesp.br>
2025-10-12 19:50:54 -0300

    refactor book list ui and add bookcard widget

--- lib/presentation/books/widgets/books_gridview_list.dart
+++ lib/presentation/books/widgets/books_gridview_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-import '../../../core/theme/custom/gridview_theme.dart';
+import './book_card.dart';
 
 class BooksGridView extends StatelessWidget {
   final List<dynamic> books;
@@ -15,7 +14,6 @@ class BooksGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context).extension<GridViewThemeData>()!;
     return GridView.builder(
       padding: EdgeInsets.fromLTRB(15, 8, 15, 80),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -27,96 +25,11 @@ class BooksGridView extends StatelessWidget {
       itemCount: books.length,
       itemBuilder: (context, index) {
         var book = books[index];
-        return GestureDetector(
+        return BookCard(
+          book: book,
           onTap: () {
             onBookTap(book['id']);
           },
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            elevation: 6,
-            shadowColor: Colors.black45,
-            clipBehavior: Clip.antiAlias,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child:
-                      (book['cover'] != null &&
-                          book['cover']['imageUrl'] != null)
-                      ? CachedNetworkImage(
-                          imageUrl: book['cover']['imageUrl'],
-                          fit: BoxFit.cover,
-                          progressIndicatorBuilder: (context, url, progress) =>
-                              Container(
-                                color: theme.cardBackgroundColor,
-                                child: Icon(
-                                  Icons.book,
-                                  color: theme.placeholderIconColor,
-                                  size: 40,
-                                ),
-                              ),
-                          errorWidget: (context, url, error) => Container(
-                            color: theme.cardBackgroundColor,
-                            child: Icon(
-                              Icons.book,
-                              color: theme.placeholderIconColor,
-                              size: 40,
-                            ),
-                          ),
-                        )
-                      : Container(
-                          color: theme.cardBackgroundColor,
-                          child: Icon(
-                            Icons.book,
-                            color: theme.placeholderIconColor,
-                            size: 40,
-                          ),
-                        ),
-                ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  height: 80,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                        bottom: Radius.circular(12),
-                      ),
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: [
-                          Colors.black.withValues(alpha: 0.9),
-                          Colors.transparent,
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 10,
-                  right: 10,
-                  bottom: 10,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        book['title'] ?? '',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: theme.titleStyle.copyWith(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
         );
       },
     );
--- lib/presentation/books/widgets/books_horizontal_listview.dart
+++ lib/presentation/books/widgets/books_horizontal_listview.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-import '../../../core/theme/custom/gridview_theme.dart';
+import './book_card.dart';
 
 class BooksHorizontalListView extends StatelessWidget {
   final List<dynamic> books;
   final void Function(String bookId) onBookTap;
+  final bool isLoading;
   final VoidCallback? onLoadMore;
 
   const BooksHorizontalListView({
     super.key,
     required this.books,
     required this.onBookTap,
+    this.isLoading = false,
     this.onLoadMore,
   });
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context).extension<GridViewThemeData>()!;
     return SizedBox(
-      height: 340,
+      height: 300,
       child: ListView.builder(
         padding: EdgeInsets.fromLTRB(15, 8, 15, 8),
         scrollDirection: Axis.horizontal,
@@ -30,115 +30,24 @@ class BooksHorizontalListView extends StatelessWidget {
               width: 200,
               margin: EdgeInsets.only(right: 5),
               child: Center(
-                child: OutlinedButton(
-                  onPressed: onLoadMore,
-                  child: Text('Carregar mais'),
-                ),
+                child: isLoading
+                    ? CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: onLoadMore,
+                        child: Text('Carregar mais'),
+                      ),
               ),
             );
           }
           var book = books[index];
-          return GestureDetector(
-            onTap: () {
-              onBookTap(book['id']);
-            },
-            child: Container(
-              margin: EdgeInsets.only(right: 5),
-              width: 200,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                elevation: 6,
-                shadowColor: Colors.black45,
-                clipBehavior: Clip.antiAlias,
-                child: Stack(
-                  children: [
-                    Positioned.fill(
-                      child:
-                          (book['cover'] != null &&
-                              book['cover']['imageUrl'] != null)
-                          ? CachedNetworkImage(
-                              imageUrl: book['cover']['imageUrl'],
-                              fit: BoxFit.cover,
-                              progressIndicatorBuilder:
-                                  (context, url, progress) => Container(
-                                    color: theme.cardBackgroundColor,
-                                    child: Icon(
-                                      Icons.book,
-                                      color: theme.placeholderIconColor,
-                                      size: 40,
-                                    ),
-                                  ),
-                              errorWidget: (context, url, error) => Container(
-                                color: theme.cardBackgroundColor,
-                                child: Icon(
-                                  Icons.book,
-                                  color: theme.placeholderIconColor,
-                                  size: 40,
-                                ),
-                              ),
-                            )
-                          : Container(
-                              color: theme.cardBackgroundColor,
-                              child: Icon(
-                                Icons.book,
-                                color: theme.placeholderIconColor,
-                                size: 40,
-                              ),
-                            ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      height: 80,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.vertical(
-                            bottom: Radius.circular(12),
-                          ),
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.black.withValues(alpha: 0.9),
-                              Colors.transparent,
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 10,
-                      right: 10,
-                      bottom: 10,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            book['title'] ?? '',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: theme.titleStyle.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            book['author'] ?? '',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: theme.authorStyle.copyWith(
-                              color: Colors.white70,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+          return Container(
+            width: 180,
+            margin: EdgeInsets.only(right: 5),
+            child: BookCard(
+              book: book,
+              onTap: () {
+                onBookTap(book['id']);
+              },
             ),
           );
         },
